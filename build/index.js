51f875b2b8b9ce1dd41f951b99a9782024a38030
mayrene <maye199226@gmail.com>
2023-09-03 20:44:34 -0300

    change package

--- build/index.js
+++ build/index.js
@@ -4,13 +4,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const diaries_1 = __importDefault(require("./routes/diaries"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
-const PORT = 3000;
-app.get('/fruit', (_, res) => {
-    console.log('frutas');
-    res.send('hgola juguetes');
+const PORT = 'https://node-api-render';
+app.get('/ying', (_, res) => {
+    res.send('yang');
 });
+app.use('/api/diaries', diaries_1.default);
 app.listen(PORT, () => {
     console.log('servidor andando');
 });
