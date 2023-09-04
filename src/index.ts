5666c849f99e94ab70c9e60e278e4e845561a7c9
mayrene <maye199226@gmail.com>
2023-09-04 14:25:03 -0300

    change package

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import express from 'express'
 import diaryRouter from './routes/diaries'
+import 'dotenv/config'
 
 const app = express()
 app.use(express.json())
-const PORT = 'https://node-api-render'
+const PORT = process.env.PORT ?? 8080
 
 app.get('/ying', (_, res) => {
   res.send('yang')
